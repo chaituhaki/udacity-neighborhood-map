37a42ef871267dc98e63b7815fb0534890b43513
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-11-16 23:37:58 -0500

    Added infowindow functionality

--- js/script.js
+++ js/script.js
@@ -10,11 +10,29 @@ function initMap() {
     center: mapCenter,
     zoom: 12
   });
+  
   for(i=0; i<locationList().length; i++){
     var marker = new google.maps.Marker({
       position: locationList()[i].coordinates(),
+      title: locationList()[i].title(),
+      type: locationList()[i].type(),
       map: map
     });
+    var infowindow = new google.maps.InfoWindow();
+    marker.addListener('click', function(){
+      populateInfoWindow(this, infowindow)
+    });
   }
-  
+  var populateInfoWindow = function(marker, infowindow){
+      if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+
+          infowindow.addListener('click', function() {
+            infowindow.marker = null;
+          });
+      }
+  }
+   
 }
