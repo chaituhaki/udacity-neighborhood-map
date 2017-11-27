5d518a76e0edd788a6ee2c84309fb488ca013b6b
chaitanya LN <chaituhaki@users.noreply.github.com>
2017-11-27 16:40:22 -0500

    Added styles to google maps

--- js/viewModel.js
+++ js/viewModel.js
@@ -2,6 +2,7 @@ ViewModel = function(){
     center = ko.observable(mapCenter);
     foursquareList = ko.observableArray([]);
     styles = ko.observableArray(mapStyles);
+    // get data in JSON format from Foursquare API using AJAX
     $.ajax({
         type: "GET",
         datatype: "json",
@@ -10,6 +11,7 @@ ViewModel = function(){
                 "client_id="+clientID+"&v=20150603&client_secret="+clientSecret,
         success: function(data){
             var JsonData = data.response.venues;
+            //parse JSON data
             for(i=0; i<15; i++){
                 item = JsonData[i];
                 foursquareList.push(new Foursquare(item));
